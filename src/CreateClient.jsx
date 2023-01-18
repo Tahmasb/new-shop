df4542655128265b063c069042e71a86f841908f
tahmasb <hggvff222@gmail.com>
2023-01-18 21:38:21 +0330

    delete env file

--- src/CreateClient.jsx
+++ src/CreateClient.jsx
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js"
 
 export const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
+  "https://juxfezqlannwuojdkkmx.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp1eGZlenFsYW5ud3VvamRra214Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzM4MDY0ODUsImV4cCI6MTk4OTM4MjQ4NX0.TLFwYY4NQ18ul7Qu96MI8XYdxESW1GJapRrjEQOlFkA"
 )
