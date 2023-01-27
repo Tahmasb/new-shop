7b1477797414ff91039b097e547c23fa4253a4f3
tahmasb <hggvff222@gmail.com>
2023-01-27 21:57:31 +0330

    edit menu

--- src/components/CartTemplate/CartTemplate.jsx
+++ src/components/CartTemplate/CartTemplate.jsx
@@ -15,77 +15,73 @@ import { formatCurrency, addProduct } from "./../../utils"
 export default function CartTemplate(props) {
   let context = useContext(ProductsContext)
   return (
-    <Fade in={true}>
-      <Grid
-        display="flex"
-        p={0.5}
-        justifyContent={"space-between"}
-        sx={{
-          flexDirection: { xs: "column", s: "row" },
-          borderBottom: "0.1px solid lightgray",
-        }}
-        style={{ background: "#fafafa" }}
+    <Grid
+      display="flex"
+      p={0.5}
+      justifyContent={"space-between"}
+      sx={{
+        flexDirection: { xs: "column", s: "row" },
+        borderBottom: "0.1px solid lightgray",
+      }}
+      style={{ background: "#fafafa" }}
+    >
+      <Link
+        to={`/product/${props.cartItem.categoryId}${props.cartItem.uniqueId}`}
       >
-        <Link
-          to={`/product/${props.cartItem.categoryId}${props.cartItem.uniqueId}`}
-        >
-          <CardMedia
-            sx={{ maxWidth: "11.5rem", margin: "auto" }}
-            component="img"
-            src={props.cartItem.img}
-            alt="product"
-          />
-        </Link>
-        <Grid
-          width="100%"
-          display="flex"
-          flexDirection={"column"}
-          rowGap={3}
-          p={1}
-          justifyContent="space-around"
-        >
-          <Grid display="flex" justifyContent={"space-between"}>
-            <Link
-              to={`/product/${props.cartItem.categoryId}${props.cartItem.uniqueId}`}
-            >
-              <Typography fontFamily={"vazir"}>
-                {props.cartItem.title}
-              </Typography>{" "}
-            </Link>
-            <Typography>{formatCurrency(props.cartItem.price)}</Typography>
-          </Grid>
-          <Grid
-            display="flex"
-            alignItems="center"
-            justifyContent={"space-between"}
+        <CardMedia
+          sx={{ maxWidth: "11.5rem", margin: "auto" }}
+          component="img"
+          src={props.cartItem.img}
+          alt="product"
+        />
+      </Link>
+      <Grid
+        width="100%"
+        display="flex"
+        flexDirection={"column"}
+        rowGap={3}
+        p={1}
+        justifyContent="space-around"
+      >
+        <Grid display="flex" justifyContent={"space-between"}>
+          <Link
+            to={`/product/${props.cartItem.categoryId}${props.cartItem.uniqueId}`}
           >
-            <Typography>{`${props.cartItem.count || ""} ${
-              props.info.num
-            }`}</Typography>
-            <ButtonGroup orientation="vertical">
-              <Button
-                onClick={() => {
-                  addProduct(context, props.cartItem)
-                }}
-                size="small"
-                color="primary"
-                variant="outlined"
-              >
-                {props.info.type}
-              </Button>
-              <Button
-                onClick={() => {
-                  props.info.func(context, props.cartItem.uniqueId)
-                }}
-                size="small"
-                color="error"
-              >
-                حذف از اینجا
-              </Button>
-            </ButtonGroup>
-          </Grid>
+            <Typography fontFamily={"vazir"}>{props.cartItem.title}</Typography>{" "}
+          </Link>
+          <Typography>{formatCurrency(props.cartItem.price)}</Typography>
+        </Grid>
+        <Grid
+          display="flex"
+          alignItems="center"
+          justifyContent={"space-between"}
+        >
+          <Typography>{`${props.cartItem.count || ""} ${
+            props.info.num
+          }`}</Typography>
+          <ButtonGroup orientation="vertical">
+            <Button
+              onClick={() => {
+                addProduct(context, props.cartItem)
+              }}
+              size="small"
+              color="primary"
+              variant="outlined"
+            >
+              {props.info.type}
+            </Button>
+            <Button
+              onClick={() => {
+                props.info.func(context, props.cartItem.uniqueId)
+              }}
+              size="small"
+              color="error"
+            >
+              حذف از اینجا
+            </Button>
+          </ButtonGroup>
         </Grid>
       </Grid>
-    </Fade>
+    </Grid>
   )
 }
